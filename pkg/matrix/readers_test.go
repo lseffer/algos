776a4ac06c22c9892aced5b03467b2589ac7b34c
Leonard Seffer <leonard.seffer@wolt.com>
2020-11-15 21:53:16 +0200

    fix redundancy

--- pkg/matrix/readers_test.go
+++ pkg/matrix/readers_test.go
@@ -20,7 +20,7 @@ func Test_parseMatrix(t *testing.T) {
 			args: args{stringArr: [][]string{{"0", "1", "1", "2", "3"},
 				{"0", "124", "14", "12", "0"},
 				{"0", "34", "4", "3", "34"}}},
-			wantRes: &DenseMatrix{Rows: []*Vector{&Vector{Values: []float64{0.0, 1.0, 1.0, 2.0, 3.0}}, &Vector{Values: []float64{0.0, 124.0, 14.0, 12.0, 0.0}}, &Vector{Values: []float64{0.0, 34.0, 4.0, 3.0, 34.0}}}},
+			wantRes: &DenseMatrix{Rows: []*Vector{{Values: []float64{0.0, 1.0, 1.0, 2.0, 3.0}}, {Values: []float64{0.0, 124.0, 14.0, 12.0, 0.0}}, {Values: []float64{0.0, 34.0, 4.0, 3.0, 34.0}}}},
 			wantErr: false,
 		},
 		{
@@ -28,7 +28,7 @@ func Test_parseMatrix(t *testing.T) {
 			args: args{stringArr: [][]string{{"0", "1", "1", "2", "3"},
 				{"0", "124", "14"},
 				{"0", "34", "4"}}},
-			wantRes: &DenseMatrix{Rows: []*Vector{&Vector{Values: []float64{0.0, 1.0, 1.0, 2.0, 3.0}}, &Vector{Values: []float64{0.0, 124.0, 14.0, 0.0, 0.0}}, &Vector{Values: []float64{0.0, 34.0, 4.0, 0.0, 0.0}}}},
+			wantRes: &DenseMatrix{Rows: []*Vector{{Values: []float64{0.0, 1.0, 1.0, 2.0, 3.0}}, {Values: []float64{0.0, 124.0, 14.0, 0.0, 0.0}}, {Values: []float64{0.0, 34.0, 4.0, 0.0, 0.0}}}},
 			wantErr: false,
 		},
 		{
@@ -36,7 +36,7 @@ func Test_parseMatrix(t *testing.T) {
 			args: args{stringArr: [][]string{{"hello", "1", "1", "2", "3"},
 				{"0", "124", "14"},
 				{"0", "34", "4"}}},
-			wantRes: &DenseMatrix{Rows: []*Vector{&Vector{Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}, &Vector{Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}, &Vector{Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}}},
+			wantRes: &DenseMatrix{Rows: []*Vector{{Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}, {Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}, {Values: []float64{0.0, 0.0, 0.0, 0.0, 0.0}}}},
 			wantErr: true,
 		},
 	}
